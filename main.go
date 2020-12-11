bfa373fcca4090dc5e5ab8eac1ac29a8e7e3a2df
Giovane <giovane.oliveira@eucard.com.br>
2020-12-11 17:39:29 -0300

    projeto-inicial

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+
+	"github.com/GiovaneVerbinnen/routes"
+	_ "github.com/lib/pq"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	routes.CarregaRotas()
+	http.ListenAndServe(":8000", nil)
+}
